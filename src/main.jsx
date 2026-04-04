7c047db3a99d94fed2892bd681a86ca58f9018f6
aliyah-lh <aliyah.lavallee.harrari@gmail.com>
2026-04-04 03:00:04 +0200

    connexion de Redux a l'application (Provider)
    - ajout du Provider de react-redux dans main.jsx
    - le Provider enveloppe toute l'application
    - tous les composants peuvent maintenant acceder au store Redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,30 @@
 // ============================================================
-// main.jsx - Point d'entree de l'application React
-// C'est le premier fichier qui s'execute quand l'app demarre
+// main.jsx - Point d'entrée de l'application React
+// C'est le premier fichier qui s'exécute quand l'app démarre
 // ============================================================
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// BrowserRouter permet de gerer la navigation (les URLs)
+// On importe BrowserRouter pour gérer la navigation (les URLs)
 import { BrowserRouter } from 'react-router-dom'
+// On importe le Provider de Redux pour rendre le store accessible partout
+import { Provider } from 'react-redux'
+// On importe notre store Redux (qu'on va créer juste après)
+import { store } from './store/store'
+// On importe notre composant principal
 import App from './App'
+// On importe le CSS global
 import './App.css'
 
 // On "monte" l'application React dans la div #root du fichier index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* Provider : permet à tous les composants d'accéder au store Redux */}
+    <Provider store={store}>
+      {/* BrowserRouter : permet d'utiliser les routes (react-router) */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
